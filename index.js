455c2781d001210de0368a05835caa276455b0b9
IsaacMusera <isaacmusera05@gmail.com>
2024-04-04 15:40:20 +0300

    sending fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configObj = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configObj)
+      .then(response => response.json())
+      .then(data => {
+        document.body.innerHTML += `<p>New ID: ${data.id}</p>`;
+      })
+      .catch(error => {
+        document.body.innerHTML += `<p>Error: ${error.message}</p>`;
+      });
+  }
+  
+  submitData("John Doe", "john@example.com");
+  
